2d33bfb347e214d998e2baed32f6ff3e93dccdfc
silvianaim02 <silvianaim68@gmail.com>
2023-11-27 09:16:51 +0700

    add frontend mentor rules

---
+++ src/page/Home.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+
+const Home = () => {
+
+    return (
+        <>
+        
+            {/* ATTRIBUTION */}
+            <div className="attribution">
+                Challenge by <Link to="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</Link>.
+                Coded by <Link to="https://silvianaim.com" target="_blank">Silvia Naim</Link>.
+            </div>
+        </>
+    )
+};
+
+export default Home;
